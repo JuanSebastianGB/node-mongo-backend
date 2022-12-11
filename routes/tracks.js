130a218494b8fc17464673d43a716b80dbc460db
Juan Sebastian Gonzalez <3393@holbertonstudents.com>
2022-12-11 15:02:11 -0500

    Adding validator for track creation

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { getItems, createItem } = require('../controllers/tracks');
+const { validatorCreateItem } = require('../validators/tracks');
 const router = express.Router();
 
 router.get('/', getItems);
-router.post('/', createItem);
+router.post('/', validatorCreateItem, createItem);
 
 module.exports = router;
